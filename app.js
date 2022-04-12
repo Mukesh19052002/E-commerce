b0778c4530b3714b5af8f8b1517ae08a2701a854
Mukesh Rao <mukesh19052002@gmail.com>
2022-04-12 21:57:47 +0530

    modified

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ const path = require('path');
 
 
 const db = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'user_list'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'users'
 });
 
-db.connect((error)=>{
-    if(error){
+db.connect((error) => {
+    if (error) {
         console.log(error);
-    }else{
+    } else {
         console.log("SQL connected");
     }
 });
@@ -27,43 +27,57 @@ app.set('view engine', 'ejs');
 
 
 //directory for css
-const publicDirectory = path.join(__dirname,'./public');
+const publicDirectory = path.join(__dirname, './public');
 app.use(express.static(publicDirectory));
 
 
 //calling functions
-app.get('/',function(req,res){
+app.get('/', function(req, res) {
     res.render('index');
 });
 
-app.get('/login',function(req,res){
+app.get('/login', function(req, res) {
     res.render('login');
 });
 
-app.get('/register',function(req,res){
+app.get('/register', function(req, res) {
     res.render('register');
 });
 
-app.post('/register-form',function(req,res){
-    const{name,email,password,passwordConfirm} = req.body;
+app.get('/homepage', function(req, res) {
+    res.render('homepage');
+});
+
+app.post('/register-form', function(req, res) {
+    const { name, email, password, passwordConfirm } = req.body;
     console.log(req.body);
-    if(password===passwordConfirm){
-        db.query("select * from user_details where email = ?",[email],function(req,res){
-            if(res.length>0){
-                // return res.json({status:'error',error:'Already exsists'});
-            }else{
-                db.query("insert into user_details set ?",{name:name,email:email,password:password},function(req,res){
-                    // return res.json({status:'error',error:'Registered Successfully'});
-                })
-                res.render('index');
+    if (password === passwordConfirm) {
+        db.query("select * from user_details where email = ?", [email], function(req, res) {
+            if (res.length > 0) {
+                res.send("Email Already Exsists");
+            } else {
+                db.query("insert into user_details set ?", { name: name, email: email, password: password }, function(req, res) {
+                    res.redirect('/homepage');
+                });
             }
-        })
-    }else{
-        // return res.json({"message":"Incorrect Password"});
+        });
+    } else {
+        res.send("Incorrect Password");
     }
 });
 
+app.post('/login-form', function(req, res) {
+    const { email, password } = req.body;
+    db.query("select * from user_details where email=?", [email], function(req, res) {
+        if (res.length > 1) {
+            console.log("success");
+            res.redirect('/homepage');
+        }
+    });
+});
+
+
 //port connection
-app.listen(8080,()=>{
+app.listen(8080, () => {
     console.log("Server Started on port 8080");
 });
